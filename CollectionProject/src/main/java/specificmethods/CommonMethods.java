6dbc596795ad943586a367f5e5256feba0f092e5
Adhitya <Adhitya@LAPTOP-5I008H6Q>
2019-12-20 09:23:06 +0530

    CheapAir testNG xml file

--- CollectionProject/src/main/java/specificmethods/CommonMethods.java
+++ CollectionProject/src/main/java/specificmethods/CommonMethods.java
@@ -2,16 +2,17 @@ package specificmethods;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.Parameters;
 
 import base.WebDriverLibrary;
 
 public class CommonMethods extends WebDriverLibrary{
 	
+	@Parameters({"url"})
 	@BeforeMethod
-	public void launchTheApplication()
+	public void launchTheApplication(String url)
 	{
-		loadURL("https://www.cheapair.com/");
+		loadURL(url);
 	}
 	@AfterMethod
 	public void closeBrowser()
